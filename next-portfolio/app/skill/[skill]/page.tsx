f8ce72635fe5b5b7e131d86f9f3faeec9dda7ac9
David Murdoch <dsmurdoch357@gmail.com>
2024-02-02 19:44:13 +0000

    feat(skill): add skill page
    * added contentful
    * added api functions

---
+++ next-portfolio/app/skill/[skill]/page.tsx
@@ -0,0 +1,44 @@
+import type { FunctionComponent } from "react";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { EmptyStar, FilledStar, Main, StyledGoBack } from "~/components";
+import { getSkillBySlug } from "~/utils/api";
+import { contentfulOptions } from "~/utils";
+
+type Props = {
+  params: {
+    skill: string;
+  };
+};
+
+const Page: FunctionComponent<Props> = async ({ params }) => {
+  const skill = await getSkillBySlug(params.skill);
+
+  const skillName = skill?.skillName;
+  const content = skill?.content;
+  const rating = skill?.rating as number;
+
+  return (
+    <Main className="w-full px-6 py-8">
+      <StyledGoBack className="mb-4" href="/#skills" />
+
+      <h1 className="text-4xl text-sky-700">{skillName}</h1>
+      <div className="mb-4">
+        {documentToReactComponents(content?.json, contentfulOptions)}
+      </div>
+
+      <div className="flex">
+        <h2 className="tracking-widest text-sky-600">PROFICIENCY:</h2>
+
+        {Array.from({ length: rating }).map((_, i) => (
+          <FilledStar className="h-6 w-6" key={`filled-star-${i}`} />
+        ))}
+
+        {Array.from({ length: 5 - rating }).map((_, i) => (
+          <EmptyStar className="h-6 w-6" key={`empty-star-${i}`} />
+        ))}
+      </div>
+    </Main>
+  );
+};
+
+export default Page;
--- next-portfolio/utils/index.ts
+++ next-portfolio/utils/index.ts
@@ -1,1 +1,2 @@
 export { default as cn } from "./class-names";
+export { default as contentfulOptions } from "./contentful-options";
