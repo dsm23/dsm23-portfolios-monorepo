3c71aaf9552d59b55763bfd67a73511bce55908a
David Murdoch <dsmurdoch357@gmail.com>
2024-02-05 17:57:27 +0000

    chore(package.json): update dependencies

--- next-portfolio/components/textarea/index.tsx
+++ next-portfolio/components/textarea/index.tsx
@@ -2,8 +2,7 @@ import { forwardRef } from "react";
 import type { TextareaHTMLAttributes } from "react";
 import { cn } from "~/utils";
 
-export interface TextareaProps
-  extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => (
