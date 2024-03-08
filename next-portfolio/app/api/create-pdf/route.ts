ee340c0567f16c7a80671714f4e957de74d13708
David Murdoch <dsmurdoch357@gmail.com>
2024-03-08 01:17:24 +0000

    chore(api/create-pdf): try VERCEL_URL instead in /api/create-pdf
    * remove NEXT_PUBLIC_, no client

--- next-portfolio/app/api/create-pdf/route.ts
+++ next-portfolio/app/api/create-pdf/route.ts
@@ -12,7 +12,7 @@ export async function GET() {
 
   const page = await browser.newPage();
 
-  const websiteUrl = `http${process.env.NODE_ENV === "development" ? "" : "s"}://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  const websiteUrl = `http${process.env.NODE_ENV === "development" ? "" : "s"}://${process.env.VERCEL_URL}`;
 
   await page.goto(websiteUrl, { waitUntil: "networkidle0" });
 
