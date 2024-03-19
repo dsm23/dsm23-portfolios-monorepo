f4e4b687c3b00bf2dd135e1bdb729953227745af
David Murdoch <dsmurdoch357@gmail.com>
2024-03-19 01:43:08 +0000

    chore(resizable panels): co-locate resizable panels to /drag-and-resize

--- next-portfolio/app/drag-and-resize/resizable/index.tsx
+++ next-portfolio/app/drag-and-resize/resizable/index.tsx
@@ -2,7 +2,7 @@
 
 import type { ComponentProps } from "react";
 import * as ResizablePrimitive from "react-resizable-panels";
-import { DragHandle } from "../svgs";
+import { DragHandle } from "~/components/svgs";
 import { cn } from "~/utils";
 
 const ResizablePanelGroup = ({
