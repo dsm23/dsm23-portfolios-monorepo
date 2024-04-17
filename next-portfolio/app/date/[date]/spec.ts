0645ae52bdd52e86b5451820989ac5596d5297ee
David Murdoch <dsmurdoch357@gmail.com>
2024-04-17 14:14:08 +0100

    feat(timestamp): add timestamp microservice example backend

---
+++ next-portfolio/app/date/[date]/spec.ts
@@ -0,0 +1,104 @@
+/**
+ * @jest-environment node
+ */
+
+import type { NextRequest } from "next/server";
+import { GET } from "./route";
+
+describe("[api: date]", () => {
+  it("unix number", async () => {
+    const res = await GET(
+      // unused
+      {} as NextRequest,
+      {
+        params: {
+          date: "1451001600000",
+        },
+      },
+    );
+
+    const json = await res.json();
+
+    expect(json).toEqual({
+      unix: 1451001600000,
+      utc: "Fri, 25 Dec 2015 00:00:00 GMT",
+    });
+  });
+
+  it("Date constructor parameter, ISO", async () => {
+    const res = await GET(
+      // unused
+      {} as NextRequest,
+      {
+        params: {
+          date: "2015-12-25",
+        },
+      },
+    );
+
+    const json = await res.json();
+
+    expect(json).toEqual({
+      unix: 1451001600000,
+      utc: "Fri, 25 Dec 2015 00:00:00 GMT",
+    });
+  });
+
+  it("Date constructor parameter, long date string, ", async () => {
+    const res = await GET(
+      // unused
+      {} as NextRequest,
+      {
+        params: {
+          date: "December 25, 2015 00:00:00",
+        },
+      },
+    );
+
+    const json = await res.json();
+
+    expect(json).toEqual({
+      unix: 1451001600000,
+      utc: "Fri, 25 Dec 2015 00:00:00 GMT",
+    });
+  });
+
+  it("Date now, ", async () => {
+    jest.useFakeTimers().setSystemTime(new Date("1970-01-01T00:00:01+0000"));
+
+    const res = await GET(
+      // unused
+      {} as NextRequest,
+      {
+        params: {
+          date: "now",
+        },
+      },
+    );
+
+    const json = await res.json();
+
+    expect(json).toEqual({
+      unix: 1000,
+      utc: "Thu, 01 Jan 1970 00:00:01 GMT",
+    });
+  });
+
+  it("Invalid date, ", async () => {
+    const res = await GET(
+      // unused
+      {} as NextRequest,
+      {
+        params: {
+          date: "foobar",
+        },
+      },
+    );
+
+    const json = await res.json();
+
+    expect(json).toEqual({
+      error: "Invalid Date",
+    });
+  });
+});
---
+++ next-portfolio/app/date/page.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from "next";
+import Link from "next/link";
+import Anchor from "~/components/anchor";
+import Main from "~/components/main";
+import GoBack from "~/components/styled-go-back";
+import ViewSource from "~/components/view-source";
+
+export const metadata: Metadata = {
+  title: "David Murdoch Portfolio - Next.js | Date",
+  description: "svelte.dev clock example in Next.js",
+  openGraph: {
+    title: "David Murdoch Portfolio - Next.js | Date",
+    description: "svelte.dev clock example in Next.js",
+  },
+};
+
+const Page = () => (
+  <Main className="w-full px-6 py-8">
+    <GoBack className="mb-4" href="/#projects" />
+    <ViewSource pathname="app/clock/page.tsx" />
+
+    <h1 className="text-4xl uppercase tracking-widest text-sky-600">
+      Timestamp Microservice
+    </h1>
+
+    <p>
+      This is an example backend microservice inspired by{" "}
+      <Anchor
+        href="https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/timestamp-microservice"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        freeCodeCamp
+      </Anchor>
+    </p>
+
+    <div className="container mx-auto mt-10">
+      <h2>Example Usage:</h2>
+      <ul>
+        <li>
+          <Anchor as={Link} href="/date/2015-12-25">
+            [project url]/date/2015-12-25
+          </Anchor>
+        </li>
+        <li>
+          <Anchor as={Link} href="/date/1451001600000">
+            [project url]/date/1451001600000
+          </Anchor>
+        </li>
+        <li>
+          <Anchor as={Link} href="/date/now">
+            [project url]/date/now
+          </Anchor>
+        </li>
+      </ul>
+
+      <h2>Example Output:</h2>
+      <p>
+        <code>
+          {JSON.stringify({
+            unix: 1451001600000,
+            utc: "Fri, 25 Dec 2015 00:00:00 GMT",
+          })}
+        </code>
+      </p>
+    </div>
+  </Main>
+);
+
+export default Page;
