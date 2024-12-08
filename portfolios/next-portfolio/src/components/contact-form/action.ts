8619a0dbf3a83d8197016e339c8ed2b46fbc5f11
David Murdoch <dsmurdoch357@gmail.com>
2024-12-08 16:39:58 +0000

    feat(contact form): add contact form

---
+++ portfolios/next-portfolio/src/components/contact-form/action.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { Resend } from "resend";
+import { EmailTemplate } from "~/components/contact-form/email";
+
+export async function send({
+  email,
+  message,
+}: {
+  email: string;
+  message: string;
+}) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  await resend.emails.send({
+    from: `Acme <onboarding@resend.dev>`,
+    to: ["dsmurdoch357@gmail.com"],
+    subject: "From next portfolio",
+    react: EmailTemplate({ email, message }),
+  });
+}
